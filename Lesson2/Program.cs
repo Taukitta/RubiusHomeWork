d531714c5e269173c04b5615e877c6a9e51217a7
antondanilov <veymz321@gmail.com>
2024-02-04 16:14:17 +0700

    Добавьте файлы проекта.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+namespace Lesson2;
+
+
+public class Program
+{
+    Console.WriteLine("Введите год!");
+    public static void Main(int.value1)
+    {
+        var value1 = Console.ReadLine()
+
+        if (value1 % 4 == 0, value1 % 400 == 0, value1 % 100 != 0)
+        {
+            Console.WriteLine("Год високостый!");
+        }
+        else
+        {
+            Console.WriteLine("Год не високосный");
+        }
+    }
+}
+
